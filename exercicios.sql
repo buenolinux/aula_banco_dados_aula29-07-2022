93422ce9e86c29b40effee84f48b0db48106f538
André <108432366+buenolinux@users.noreply.github.com>
2022-07-29 14:18:28 -0300

    Update exercicios.sql

--- exercicios.sql
+++ exercicios.sql
@@ -9,3 +9,17 @@ AS
 SELECT fone, email FROM tab_professores;
 
 EXECUTE spMostraFoneEmailProfessores;
+
+/*
+Criar uma SP para aumentar o salário de todos os professores com qualquer porcentagem
+*/
+CREATE PROCEDURE spSomaSalario 
+
+@teste  decimal(10,2)
+AS
+UPDATE tab_professores SET salario = salario * @teste;
+SELECT * FROM tab_professores;
+
+EXECUTE spSomaSalario 1.10;
+
+SELECT * FROM tab_professores;
