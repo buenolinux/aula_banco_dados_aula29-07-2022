bf72102d6c2772a6da6ee6a574f76ead3ab75c3b
André <108432366+buenolinux@users.noreply.github.com>
2022-07-29 17:10:39 -0300

    Update exercicios.sql

--- exercicios.sql
+++ exercicios.sql
@@ -110,4 +110,9 @@ TRIGGER é como uma função que é disparada mediante alguma ação.
 Geralmente essas ações que disparam os TRIGGERS são alterações
 nas tabelas por meio de operações de inserção, exclusão e 
 atualização de dados (insert, delete e update).
+
+Vamos a um exemplo prático, criaremos um controle de fluxode caixa
+de uma loja, onde a cada venda inserida na tabela de vendas,
+será automaticamente atualizada na tabela de caixa
+o mesmo acontecerá quando removermos a venda.
 */
